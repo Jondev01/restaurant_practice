d8d526a7958355ce302c07304dab311119841cdc
Jonathan Wernersson <jonathan2008@live.se>
2018-08-25 00:07:02 +0200

    Trying to recreate design from html template from w3school

---
+++ main.js
@@ -0,0 +1,30 @@
+var displayFood = document.getElementById('display-food');
+var displayDrink = document.getElementById('display-drink');
+var foodMenu = document.getElementById('food-menu');
+var drinkMenu = document.getElementById('drink-menu');
+var bodyStyle =  window.getComputedStyle(document.body);
+var bgColor = bodyStyle.getPropertyValue("background-color");
+var textColor = bodyStyle.getPropertyValue("color");
+var clickedColor = '#666';
+
+displayFood.addEventListener("click", displayFoodMenu);
+displayDrink.addEventListener("click", displayDrinkMenu);
+
+function displayFoodMenu(){
+	displayDrink.style.backgroundColor = bgColor;
+	displayDrink.style.color = textColor;
+	displayFood.style.backgroundColor = clickedColor;
+	displayFood.style.color = bgColor;
+	drinkMenu.style.display="none";
+	foodMenu.style.display = "block";
+}
+
+function displayDrinkMenu(){
+	displayFood.style.backgroundColor = bgColor;
+	displayFood.style.color = textColor;
+	displayDrink.style.backgroundColor = clickedColor;
+	displayDrink.style.color = bgColor;
+	foodMenu.style.display = "none";
+	drinkMenu.style.display = "block";
+
+}
